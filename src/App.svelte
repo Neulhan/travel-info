364ba26ec3b2a038cc68842ee60316c7d63d7561
동현 김 <akainoo@naver.com>
2021-01-16 18:16:24 +0900

    feat : 해커톤 list 부분 완성

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <Router>
-  <Route path="" component={Main}></Route>
+  <Route path="/list" component={Main}></Route>
   <Route path="/map" component={Map}></Route>
-  <Route path="/list" component={List}></Route>
+  <Route path="" component={List}></Route>
 </Router>
